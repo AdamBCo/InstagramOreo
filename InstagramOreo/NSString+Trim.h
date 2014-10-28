29426aa7d50ffb3be3d823b243ecbcdf402e4423
xeaza <taylorwrightsanson@gmail.com>
2014-10-27 21:39:26 -0500

    Added Category to Trim whitespace from NSString

---
+++ InstagramOreo/NSString+Trim.h
@@ -0,0 +1,15 @@
+//
+//  NSString+Trim.h
+//  InstagramOreo
+//
+//  Created by Taylor Wright-Sanson on 10/27/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Trim)
+
+- (NSString *)trim;
+
+@end
