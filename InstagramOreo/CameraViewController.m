86438568b1a3cfeabcdc8f690eb00bd52abbacc3
Adam Cooper <acooper@rallydev.com>
2014-10-27 22:35:49 -0500

    IMAGES!!

--- InstagramOreo/CameraViewController.m
+++ InstagramOreo/CameraViewController.m
@@ -7,10 +7,13 @@
 //
 
 #import "CameraViewController.h"
+#import <Parse/Parse.h>
 
 @interface CameraViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 @property UIImagePickerController *imagePicker;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+@property BOOL takePhoto;
 
 @end
 
@@ -19,18 +22,29 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
-    picker.delegate = self;
-    picker.allowsEditing = YES;
-    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
-    
-    [self presentViewController:picker animated:YES completion:NULL];
+    self.takePhoto = YES;
     
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
+    if (self.takePhoto == YES) {
+        UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+        picker.delegate = self;
+        picker.allowsEditing = YES;
+        picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        
+        [self presentViewController:picker animated:YES completion:NULL];
+        self.takePhoto = NO;
+    }
     
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch * touch = [touches anyObject];
+    if(touch.phase == UITouchPhaseBegan) {
+        [self.textView resignFirstResponder];
+    }
 }
 
 //- (IBAction)selectPhoto:(UIButton *)sender {
@@ -54,10 +68,49 @@
     
 }
 
+-(void)uploadImage{
+
+    NSData *imageData = UIImageJPEGRepresentation(self.imageView.image, 0.05f);
+    
+    PFFile *imageFile = [PFFile fileWithName:@"Image.jpg" data:imageData];
+
+    [imageFile saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (!error) {
+            
+            PFObject *userPhoto = [PFObject objectWithClassName:@"UserPhoto"];
+            [userPhoto setObject:imageFile forKey:@"imageFile"];
+            
+            userPhoto.ACL = [PFACL ACLWithUser:[PFUser currentUser]];
+            
+            PFUser *user = [PFUser currentUser];
+            [userPhoto setObject:user forKey:@"user"];
+            
+            [userPhoto saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                if (!error) {
+                    
+                    NSLog(@"IT WAS Posted!!");
+                    //THis might work!!
+                    [self.navigationController popToRootViewControllerAnimated:YES];
+                    self.takePhoto = YES;
+                }
+                else{
+                    NSLog(@"Error: %@ %@", error, [error userInfo]);
+                }
+            }];
+        }
+        else{
+            NSLog(@"Error: %@ %@", error, [error userInfo]);
+        }
+    }];
+}
+
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
     
     [picker dismissViewControllerAnimated:YES completion:NULL];
     
+}
+- (IBAction)postImage:(id)sender {
+    [self uploadImage];
 }
 
 
--- InstagramOreo/NewsFeedTableViewController.m
+++ InstagramOreo/NewsFeedTableViewController.m
@@ -29,6 +29,7 @@
     if (currentUser) {
         NSLog(@"The current user is: %@", currentUser.username);
         [self.tabBarController.tabBar setHidden:NO];
+        [self downloadAllImages];
     }
     else {
         [self performSegueWithIdentifier:@"ShowLoginSegue" sender:self];
@@ -56,9 +57,39 @@
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
     NewsFeedTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"NewsFeedCell"];
+    cell.capturedPhoto.image = [UIImage imageWithData:[self.photoPosts objectAtIndex:indexPath.row]];
     return cell;
 }
 
+- (void)downloadAllImages
+{
+    PFQuery *query = [PFQuery queryWithClassName:@"UserPhoto"];
+    PFUser *user = [PFUser currentUser];
+    [query whereKey:@"user" equalTo:user];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        NSMutableArray *newObjectIDArray = [NSMutableArray array];
+        NSMutableArray *photoArray = [NSMutableArray array];
+        
+        if (objects.count > 0) {
+            for (PFObject *eachObject in objects) {
+                [newObjectIDArray addObject:[eachObject objectId]];
+                NSLog(@"PFObject HAHA: %@",eachObject);
+                PFFile *file = [eachObject objectForKey:@"imageFile"];
+                NSLog(@"Notifications: %@",file);
+                NSLog(@"Data: %@", [file getData]);
+                NSData *data = [file getData];
+                [photoArray addObject:data];
+            }
 
+            self.photoPosts = [NSArray arrayWithArray:photoArray];
+            [self.tableView reloadData];
+        }
+    }];
+}
+
+
+                
+                
 @end
